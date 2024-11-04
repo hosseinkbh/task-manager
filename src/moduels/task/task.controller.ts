74b029c768e95186d5682942def289ad282b3f43
hossein khodabandeh <hosseinkhodabandeh01@gmail.com>
2024-11-04 16:09:48 +0330

    feat(task module): adding new features for task module

--- src/moduels/task/task.controller.ts
+++ src/moduels/task/task.controller.ts
@@ -1,65 +1,72 @@
-import {
-  Body,
-  Controller,
-  Get,
-  Param,
-  Post,
-  Put,
-  Query,
-  Session,
-  UseGuards,
-} from "@nestjs/common";
-import {
-  CreateTaskDto,
-  FilterListTasksDto,
-  MongoIdDto,
-  UpdateTaskDto,
-} from "./task.dto";
-import { Types } from "mongoose";
-import { TaskService } from "./task.service";
-import { SessionType } from "../../types/type";
-import { AuthGuard } from "../../gurds/custom.auth.guard";
-import { I18nService } from "nestjs-i18n";
-import { PriorityEnum, StatuEnum } from "../../models/task.model";
+import {Body, Controller, Get, Param, Post, Put, Query, Redirect, Render, Session,} from "@nestjs/common";
+import {CreateTaskDto, FilterListTasksDto, MongoIdDto, UpdateTaskDto,} from "./task.dto";
+import {TaskService} from "./task.service";
+import {SessionType} from "../../types/type";
+import {I18nService} from "nestjs-i18n";
+import {PriorityEnum, StatuEnum} from "../../models/task.model";
 
 // @UseGuards(AuthGuard)
 @Controller("task")
 export class TaskController {
-  constructor(
-    private readonly taskService: TaskService,
-    private readonly i18nService: I18nService
-  ) {}
-  @Render("create")
-  @Get("view/create")
-  async createTaskView() {
-    const users = await this.taskService.CreateTaskUsers();
-    return { PriorityEnum: PriorityEnum, users: users };
-  }
+    constructor(
+        private readonly taskService: TaskService,
+        private readonly i18nService: I18nService
+    ) {
+    }
 
-  @Redirect("/task/board")
-  @Post("create")
-  async createTask(
-    @Session() session: SessionType,
-    @Body() body: CreateTaskDto
-  ) {
-    await this.taskService.createTask(body, session);
-  }
-  @Put("/update/:id")
-  async updateTask(
-    @Session() session: SessionType,
-    @Param() { id }: MongoIdDto,
-    @Body() body: UpdateTaskDto
-  ) {
-    return this.taskService.updateTask(id, body, session);
-  }
-  @Render("board")
-  @Get("/board")
-  async listTasks(@Query() filter: FilterListTasksDto) {
-    const tasks = await this.taskService.listTasks(filter);
-    return { tasks, StatuEnum };
-  }
-  @Get("/assignes")
-  async listAssigneTasks(@Session() session: SessionType) {
-    return this.taskService.listAssigneTasks(session);
-  }
+    @Render("create")
+    @Get("view/create")
+    async createTaskView() {
+        const users = await this.taskService.CreateTaskUsers();
+        return {PriorityEnum: PriorityEnum, users: users};
+    }
+
+    @Get("/get/:id")
+    async getTask(
+        @Param() {id}: MongoIdDto,
+    ) {
+        return this.taskService.getTask(id);
+    }
+
+    @Redirect("/task/board")
+    @Post("create")
+    async createTask(
+        @Session() session: SessionType,
+        @Body() body: CreateTaskDto
+    ) {
+        await this.taskService.createTask(body, session);
+    }
+
+    @Render("edit")
+    @Get("/view/edit/:id")
+    async updateTaskView(
+        @Session() session: SessionType,
+        @Param() {id}: MongoIdDto,
+    ) {
+        const users = await this.taskService.CreateTaskUsers();
+
+        const task = await this.taskService.getTask(id);
+        return {task: task, PriorityEnum: PriorityEnum, users: users}
+    }
+
+    @Put("/update/:id")
+    async updateTask(
+        @Session() session: SessionType,
+        @Param() {id}: MongoIdDto,
+        @Body() body: UpdateTaskDto
+    ) {
+        return this.taskService.updateTask(id, body, session);
+    }
+
+    @Render("board")
+    @Get("/board")
+    async listTasks(@Query() filter: FilterListTasksDto) {
+        const tasks = await this.taskService.listTasks(filter);
+        return {tasks, StatuEnum};
+    }
+
+    @Get("/assignes")
+    async listAssigneTasks(@Session() session: SessionType) {
+        return this.taskService.listAssigneTasks(session);
+    }
 }

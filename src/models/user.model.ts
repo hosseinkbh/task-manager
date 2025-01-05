d205f6fb2d7ea9c07df4ed09d684f25b61e97823
hossein khodabandeh <hosseinkhodabandeh01@gmail.com>
2025-01-05 09:58:25 +0330

    feat(model): adding mongoose aggregate paginate to user model

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,25 +1,30 @@
-import { ModelDefinition, Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
-import { Types, HydratedDocument } from "mongoose";
+import {ModelDefinition, Prop, Schema, SchemaFactory} from "@nestjs/mongoose";
+import {HydratedDocument, SchemaTimestampsConfig} from "mongoose";
 import paginate from "mongoose-paginate-v2";
 
-@Schema({ collection: "UserModel", timestamps: true, versionKey: false })
+const mongooseAggregatePaginate = require("mongoose-aggregate-paginate-v2");
+
+
+@Schema({collection: "UserModel", timestamps: true, versionKey: false})
 export class UserModel {
-  @Prop({ type: String, minlength: 2, maxlength: 20 })
-  first_name!: string;
-  @Prop({ type: String, minlength: 2, maxlength: 20 })
-  last_name!: string;
-  @Prop({ type: String, minlength: 11, maxlength: 11 })
-  phoneNumber!: string;
-  @Prop({ type: String, minlength: 10, maxlength: 50 })
-  email!: string;
-  @Prop({ type: String, minlength: 10, maxlength: 150 })
-  password!: string;
+    @Prop({type: String, minlength: 2, maxlength: 20})
+    first_name!: string;
+    @Prop({type: String, minlength: 2, maxlength: 20})
+    last_name!: string;
+    @Prop({type: String, minlength: 11, maxlength: 11})
+    phoneNumber!: string;
+    @Prop({type: String, minlength: 10, maxlength: 50})
+    email!: string;
+    @Prop({type: String, minlength: 10, maxlength: 150})
+    password!: string;
 }
 
 export const UserSchema = SchemaFactory.createForClass(UserModel);
-export type UserDocument = HydratedDocument<UserModel>;
+UserSchema.plugin(mongooseAggregatePaginate);
 UserSchema.plugin(paginate);
+
+export type UserDocument = HydratedDocument<UserModel & SchemaTimestampsConfig>;
 export const UserModelDefinition: ModelDefinition = {
-  name: UserModel.name,
-  schema: UserSchema,
+    name: UserModel.name,
+    schema: UserSchema,
 };
